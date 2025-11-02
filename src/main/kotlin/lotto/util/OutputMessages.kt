b7459c366281fcf19285fe94868bb8b7bddd8a81
NADAisWATCHING <145518496+nadajinny@users.noreply.github.com>
2025-11-02 17:36:22 +0900

    feat(output_message): 출력해야하는 문구에 대해서 정리

---
+++ src/main/kotlin/lotto/util/OutputMessages.kt
@@ -0,0 +1,9 @@
+package lotto.util
+
+object OutputMessages {
+    const val PROMPT_PURCHASE_PAYMENT = "구입금액을 입력해 주세요."
+    const val PROMPT_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
+    const val PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+    const val PROMPT_PURCHASE_AMOUNT = "개를 구매했습니다."
+    const val PROMPT_LOTTO_RESULT = "당첨 통계\n---"
+}
