fee47198806ed07ed87a060d0ff664e3b9aa94e2
NADAisWATCHING <145518496+nadajinny@users.noreply.github.com>
2025-11-03 11:02:23 +0900

    feat(calculateProfit): 로또 결과 확인

--- src/main/kotlin/lotto/util/ProfitCalculator.kt
+++ src/main/kotlin/lotto/util/ProfitCalculator.kt
@@ -1,4 +1,20 @@
 package lotto.util
 
+import lotto.model.Rank
+
 class ProfitCalculator {
+    fun rankCount(ranks: List<Rank>): Map<Rank, Int> {
+        return ranks.groupingBy { it }.eachCount()
+    }
+
+    fun totalPrice(ranks: List<Rank>): Int {
+        return ranks.sumOf { it.prize }
+    }
+
+    fun rate(ranks: List<Rank>, gameCnt: Int): Double {
+        val total = totalPrice(ranks)
+        val spend = gameCnt* LottoConfig.LottoPrice
+
+        return total / spend.toDouble() * 100.0
+    }
 }
