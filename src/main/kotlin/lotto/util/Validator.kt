2df90b530776a9452ff3a5ef5d515f6a58d60252
NADAisWATCHING <145518496+nadajinny@users.noreply.github.com>
2025-11-03 09:33:30 +0900

    feat(LottoValidator): 적절성 판별

--- src/main/kotlin/lotto/util/Validator.kt
+++ src/main/kotlin/lotto/util/Validator.kt
@@ -1,30 +1,31 @@
 package lotto.util
 
-import lotto.util.ErrorMessages
-import lotto.util.ErrorMessages.INVALID_BONUS_NUMBER
+import lotto.model.Lotto
 
 object Validator {
-    var LottoPrice = 1000
+
     fun ofPayment(input: String): Int {
         require(input.isNotBlank()) { ErrorMessages.PAYMENT_BLANK }
         val payment = input.toInt()
-        require(payment >= LottoPrice) { ErrorMessages.PAYMENT_LESS_THAN_1000 }
+        require(payment >= LottoConfig.LottoPrice) { ErrorMessages.PAYMENT_LESS_THAN_1000 }
         return payment
     }
 
-    fun ofWinningNumber(input: String): List<Int> {
+    fun ofWinningNumber(input: String): Lotto {
         require(input.isNotBlank()) { ErrorMessages.WINNING_NUMBER_BLANK }
         val number = input.split(",").mapNotNull { it.trim().toIntOrNull() }
-        require(number.size == 6) { ErrorMessages.WINNING_NUMBER_COUNT_ERROR }
-        require(number.all { it in 1..45 }) { ErrorMessages.INVALID_WINNING_NUMBER }
-        require(number.distinct().size == 6) { ErrorMessages.WINNING_NUMBER_REPETE }
-        return number
+        //수정 필요 -> size를 두번 확인함.
+        require(number.size == LottoConfig.LOTTO_SIZE) { ErrorMessages.WINNING_NUMBER_COUNT_ERROR }
+        require(number.all { it in LottoConfig.startNum..LottoConfig.endNum }) { ErrorMessages.INVALID_WINNING_NUMBER }
+        require(number.distinct().size == LottoConfig.LOTTO_SIZE) { ErrorMessages.WINNING_NUMBER_REPETE }
+        val winningNumber = Lotto(number)
+        return winningNumber
     }
 
-    fun ofBonusNumber(input: String, winningNumber: List<Int>): Int {
+    fun ofBonusNumber(input: String, winningNumber: Lotto): Int {
         require(input.isNotBlank()) { ErrorMessages.BONUS_NUMBER_BLANK }
-        val bonus = input.toIntOrNull() ?: throw IllegalArgumentException(INVALID_BONUS_NUMBER)
-        require(bonus in 1..45) { ErrorMessages.INVALID_BONUS_NUMBER }
+        val bonus = input.toIntOrNull() ?: throw IllegalArgumentException(ErrorMessages.INVALID_BONUS_NUMBER)
+        require(bonus in LottoConfig.startNum..LottoConfig.endNum) { ErrorMessages.INVALID_BONUS_NUMBER }
         require(!winningNumber.contains(bonus)) { ErrorMessages.BONUS_NUMBER_SAME_WINNING_NUMBER }
         return bonus
     }
