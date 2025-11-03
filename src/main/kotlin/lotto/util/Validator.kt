5f365ab3a5766734ee00269d12adc3d48e3890cf
NADAisWATCHING <145518496+nadajinny@users.noreply.github.com>
2025-11-03 11:00:34 +0900

    refactoring(error catch position): 에러 파악하는 위치를 Lotto에서 처리하도록 수정

--- src/main/kotlin/lotto/util/Validator.kt
+++ src/main/kotlin/lotto/util/Validator.kt
@@ -6,7 +6,7 @@ object Validator {
 
     fun ofPayment(input: String): Int {
         require(input.isNotBlank()) { ErrorMessages.PAYMENT_BLANK }
-        val payment = input.toInt()
+        val payment = input.toIntOrNull() ?: throw IllegalArgumentException(ErrorMessages.PAYMENT_FORMAT_INVALID)
         require(payment >= LottoConfig.LottoPrice) { ErrorMessages.PAYMENT_LESS_THAN_1000 }
         return payment
     }
@@ -14,10 +14,6 @@ object Validator {
     fun ofWinningNumber(input: String): Lotto {
         require(input.isNotBlank()) { ErrorMessages.WINNING_NUMBER_BLANK }
         val number = input.split(",").mapNotNull { it.trim().toIntOrNull() }
-        //수정 필요 -> size를 두번 확인함.
-        require(number.size == LottoConfig.LOTTO_SIZE) { ErrorMessages.WINNING_NUMBER_COUNT_ERROR }
-        require(number.all { it in LottoConfig.startNum..LottoConfig.endNum }) { ErrorMessages.INVALID_WINNING_NUMBER }
-        require(number.distinct().size == LottoConfig.LOTTO_SIZE) { ErrorMessages.WINNING_NUMBER_REPETE }
         val winningNumber = Lotto(number)
         return winningNumber
     }
