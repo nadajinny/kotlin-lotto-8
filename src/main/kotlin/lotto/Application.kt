d868df0a998848471cb41bb753c9700b7a2bd61d
NADAisWATCHING <145518496+nadajinny@users.noreply.github.com>
2025-11-03 10:59:44 +0900

    fix(init state): inputView, outputView 전달 누락 해결

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,9 +1,14 @@
 package lotto
 
 import lotto.controller.LottoController
+import lotto.view.ConsoleInputView
+import lotto.view.ConsoleOutputView
 
 fun main() {
     // TODO: 프로그램 구현
-    val controller = LottoController()
+    val inputView = ConsoleInputView()
+    val outputView = ConsoleOutputView()
+
+    val controller = LottoController(inputView, outputView)
     controller.run()
 }
