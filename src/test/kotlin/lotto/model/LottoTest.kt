da0db266354883d346879f85d05333fae9a13366
NADAisWATCHING <145518496+nadajinny@users.noreply.github.com>
2025-11-03 12:01:23 +0900

    refactoring(fileReposition): 파일 위치 수정

--- src/test/kotlin/lotto/model/LottoTest.kt
+++ src/test/kotlin/lotto/model/LottoTest.kt
@@ -1,6 +1,5 @@
-package lotto
+package lotto.model
 
-import lotto.model.Lotto
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -12,6 +11,13 @@ class LottoTest {
         }
     }
 
+    @Test
+    fun `로또 번호의 개수가 6개보다 적으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
+        }
+    }
+
     // TODO: 테스트가 통과하도록 프로덕션 코드 구현
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
@@ -20,5 +26,10 @@ class LottoTest {
         }
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    fun `로또 번호가 1~45 숫자 사이의 값이 아닐 경우 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 47, 523, 5))
+        }
+    }
 }
