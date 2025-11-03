803088c3ed0fc3cf3803b156aff39e7b4a89cf9b
NADAisWATCHING <145518496+nadajinny@users.noreply.github.com>
2025-11-03 09:32:41 +0900

    feat(LottoMachine): 로또 생성기 분리

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -1,4 +1,17 @@
 package lotto.model
 
-class LottoMachine {
+import camp.nextstep.edu.missionutils.Randoms
+import lotto.util.LottoConfig
+import lotto.view.ConsoleOutputView
+
+//로또를 만드는 일만 작업
+object LottoMachine {
+    fun createLottos(cnt: Int): List<Lotto> {
+        val Lottos = (1..cnt).map {
+            Lotto(Randoms.pickUniqueNumbersInRange(LottoConfig.startNum, LottoConfig.endNum, LottoConfig.LOTTO_SIZE))
+        }
+        val output = ConsoleOutputView()
+        Lottos.forEach{ myLotto -> output.printLottoNumbers(myLotto.getNumbers().joinToString(",", "[", "]")) }
+        return Lottos
+    }
 }
