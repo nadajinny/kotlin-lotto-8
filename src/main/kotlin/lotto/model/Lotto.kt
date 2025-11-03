1808f23f928f6ec7b701109d6d57fb38028bc5b9
NADAisWATCHING <145518496+nadajinny@users.noreply.github.com>
2025-11-03 11:05:19 +0900

    feat(LottoNumberErrorCheck): 로또에 관련된 오류 문제를 클래스 init에서 해결

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,20 +1,27 @@
 package lotto.model
 
+import lotto.util.ErrorMessages
+import lotto.util.LottoConfig
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.size == 6) { ErrorMessages.WINNING_NUMBER_COUNT_ERROR }
+        require(numbers.all { it in LottoConfig.startNum..LottoConfig.endNum }) { ErrorMessages.INVALID_WINNING_NUMBER }
+        require(numbers.distinct().size == LottoConfig.LOTTO_SIZE) { ErrorMessages.WINNING_NUMBER_REPETE }
     }
 
-    fun match(winningNumber: Lotto) : Int {
-        return numbers.count { it in winningNumber.numbers }
+    fun match(winningNumber: Lotto, bonusNumber: Int) : Rank {
+        val winningMatch = numbers.count { it in winningNumber.numbers }
+        val bonusMatch = bonusNumber in numbers
+        return Rank.of(winningMatch, bonusMatch)
     }
 
     fun contains(number: Int) : Boolean {
         return number in numbers
     }
 
-    fun getNumbers() : List<Int> {
-        return numbers
+    fun getNumbers() : String {
+        return numbers.joinToString(", ", "[", "]")
     }
 
     // TODO: 추가 기능 구현
